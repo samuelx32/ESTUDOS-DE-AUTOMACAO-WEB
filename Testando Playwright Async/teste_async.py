a67d2799d62cc280ba8c8b42ec90e5ffd293897e
Samuel Sobreira <samuel.sobreira@camara.leg.br>
2024-04-24 15:15:51 -0300

    Estudo Async

---
+++ Testando Playwright Async/teste_async.py
@@ -0,0 +1,91 @@
+import playwright 
+import xlwings as xw
+import pyautogui as auto
+import os
+import asyncio
+import threading
+import time
+
+
+async def auto1 (wb):
+    Diretorio = os.path.dirname(__file__)
+    arquivo=os.path.basename(__file__)[:-3]
+
+    from playwright.async_api import async_playwright, expect
+    async with async_playwright() as p:        
+        browser = await p.chromium.launch(args=["--window-position=100,100"],channel="chrome", headless=False, downloads_path=Diretorio)    
+        
+        #time.sleep(4)
+        CEP = await browser.new_page()
+        CEP.set_default_timeout(5000)
+
+        await CEP.set_viewport_size({'width': 700, 'height': 800})
+        await CEP.goto("https://www.workana.com/pt/jobs?language=pt&skills=python") 
+    
+        qtn = await CEP.locator("#projects > .project-item").count()
+        planilha = wb.sheets['Resultados']
+        for i in range(0,qtn):
+            titulo = await CEP.locator(f"#projects > .project-item > div > h2 > a > span >> nth = {i}").inner_text()
+            planilha['workana'][i+1].value = titulo
+            
+            
+
+
+async def auto2 (wb):
+    Diretorio = os.path.dirname(__file__)
+    arquivo=os.path.basename(__file__)[:-3]
+
+    from playwright.async_api import async_playwright, expect
+    async with async_playwright() as p:        
+        browser = await p.chromium.launch(args=["--window-position=900,100"],channel="chrome", headless=False, downloads_path=Diretorio)    
+        
+        #time.sleep(4)
+        page = await browser.new_page()
+        page.set_default_timeout(5000)
+
+        await page.set_viewport_size({'width': 700, 'height': 800})
+        await page.goto("https://camaranet.camara.leg.br/web/noticias-da-casa/noticias/-/resultados/avisos/10131/384295")  
+        
+        qtn = await page.locator(".resultados > li").count()
+        planilha = wb.sheets['Resultados']
+        for i in range(0,qtn):
+            noticia = await page.locator(f".resultados > li >> nth = {i} ").inner_text()
+            planilha['camara'][i+1].value = noticia
+            
+            
+            
+
+
+async def rodar():    
+    diretorio = os.path.dirname(__file__)
+    arquivo = os.path.basename(__file__)[:-3]
+
+    dir = f'{diretorio}\\{arquivo}.xlsm'
+    wb = xw.Book(dir)  
+
+    await asyncio.gather(
+        auto1(wb),
+        auto2(wb)
+    )
+    
+    
+
+    #ou
+    #tarefa1=asyncio.create_task(auto1(wb))  
+    #tarefa2=asyncio.create_task(auto2(wb))  
+
+    #await tarefa1
+    #await tarefa2
+    
+    
+
+
+
+
+def main():
+    asyncio.run(rodar())
+    
+ 
+
+if __name__ == '__main__':
+    main()
