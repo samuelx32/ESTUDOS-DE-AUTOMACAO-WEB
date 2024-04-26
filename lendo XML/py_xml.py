bab054699ce025db8b73a0445df7e35d3d860916
Samuel Sobreira <samuel.sobreira@camara.leg.br>
2024-04-26 16:23:20 -0300

    estudos sobre xml

---
+++ lendo XML/py_xml.py
@@ -0,0 +1,42 @@
+import pandas as pd
+import os
+import sys
+import xml.etree.ElementTree as ET
+import re
+
+def extrair_nome(string):
+    padrao = r'MOTORISTA:+(.*?)CPF'
+    correspondencia = re.search(padrao, string)
+    if correspondencia:
+        return correspondencia.group(1)
+    else:
+        return None
+    
+def extrair_carreta(string):
+    padrao = r'PLACA DA CARRETA:+(.*?)PLACA DO CAVALO:'
+    correspondencia = re.search(padrao, string)
+    if correspondencia:
+        return correspondencia.group(1)
+    else:
+        return None
+    
+def extrair_cavalo(string):
+    padrao = r'PLACA DO CAVALO:+(.*?)CNPJ'
+    correspondencia = re.search(padrao, string)
+    if correspondencia:
+        return correspondencia.group(1)
+    else:
+        return None
+    
+
+
+dir = os.path.dirname(__file__)
+root = ET.parse(f'{dir}\\arqxml.xml').getroot()
+nsNFE = {'ns': 'http://www.portalfiscal.inf.br/nfe'}
+dados = root.find('ns:NFe/ns:infNFe/ns:infAdic/ns:infCpl',nsNFE).text
+
+nome = extrair_nome(dados)
+carreta = extrair_carreta(dados)
+cavalo = extrair_cavalo(dados)
+
+print("\nNome: ",nome,"\nCarreta: ",carreta,"\nCavalo: ",cavalo)
